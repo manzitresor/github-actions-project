8a920025c6a63d9285656e6cc3e4c56f1c526b8d
manzitresor <manzitre@gmail.com>
2026-02-02 10:01:20 +0200

    initial commit

---
+++ src/main/java/com/thegym/githubactionsproject/GithubActionsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.thegym.githubactionsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thegym/githubactionsproject/GithubActionsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thegym.githubactionsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
